84745f0e9dad82431c9763ab4395c61e3f3d05bf
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-07 20:17:53 -0300

    initial commit

---
+++ aula04/atividades04_pt1.py
@@ -0,0 +1,10 @@
+#Atividade 01 ------------------------------------------------------------------------------------------------
+
+price = float(input('Informe o preço da compra: '))
+if price > 250:
+    desc = price * 0.16
+    new_price = price - desc
+    print('\nSua compra foi maior que R$250,00.')
+    print(f'Você ganhou 16% de desconto. Total a pagar {new_price:.2f}.')
+else: 
+    print(f'Sua compra ficou em R${price}.')
---
+++ aula04/exemplo04.py
@@ -0,0 +1,34 @@
+# #Estruturas de condição (if, elif, else)
+
+# #Ex01.:
+# time = input('Digite seu time: ').lower()
+
+# if time == 'flamengo':
+#     print('Você torce pro maior do RJ')
+
+# if time == 'nenhum':
+#     print('tsc...')
+
+# else: 
+#     print('Seu time nao é tão grande assim...')
+
+# #Ex02.:
+# idade = int(input('Informe sua idade: '))
+
+# if idade < 18:
+#     print('Você é menor de idade. Não pode entrar.')
+
+# else:
+#     print('Você é maior de idade. Pode entrar.')
+
+#Ex03.:
+points = int(input('Informe sua pontuação: '))
+
+if points >= 100:
+    print('Excelente!')
+elif points >= 50:
+    print('Bom desempenho!')
+elif points >= 25:
+    print('Satisfatório.')
+else:
+    print('Pratique mais')
