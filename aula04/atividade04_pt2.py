04ef60b6b1c87c7f344e167c1efd703bc86ca39b
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-07 21:29:18 -0300

    ajuste atividadept2.py

--- aula04/atividade04_pt2.py
+++ aula04/atividade04_pt2.py
@@ -1,7 +1,6 @@
 #Atividade 02 ------------------------------------------------------------------------------------------------
 price = float(input('Informe o preço da compra: '))
-print('[1] - À vista\n[2] - Parcelado')
-payment = int(input('Selecione a forma de pagamento: '))
+payment = int(input('\nOpções de compra:\n[1] - À vista\n[2] - Parcelado\n\nSelecione a forma de pagamento: '))
 
 if price > 250 and payment == 1:
     desc = price * 0.16
@@ -9,4 +8,4 @@ if price > 250 and payment == 1:
     print('\nSua compra foi maior que R$250,00.')
     print(f'Você ganhou 16% de desconto. Total a pagar R${new_price:.2f}.')
 else: 
-    print(f'Sua compra ficou em R${price:.2f}.')
+    print(f'\nSua compra ficou em R${price:.2f}.')
